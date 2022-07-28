eb7e9f1d0b69d975ee8afbd5a2061f58c161e5c5
Ridgewell <ridgewell@ava.solutions>
2022-07-28 21:54:43 +0200

    add tests

--- WongaTestProject/WongaGlobalTest.cs
+++ WongaTestProject/WongaGlobalTest.cs
@@ -1,12 +1,108 @@
+using ServiceB;
+
 namespace WongaTestProject
 {
     public class WongaGlobalTest
     {
 
         #region IsValidMessage Tests
+        /// <summary>
+        /// Test with a valid string
+        /// </summary>
         [Fact]
-        public void Test1()
+        public void ValidMessage()
         {
+            // Arrange
+            string testName = "Hello my name is, Ridgewell";
+
+            // Act
+            bool result = ServiceB.GetMessage.IsValidMessage(testName);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        /// <summary>
+        /// Test with a random string
+        /// </summary>
+        [Fact]
+        public void InvalidMessage()
+        {
+            // Arrange
+            string testString = "This string is invalid";
+            // Act
+            bool result = ServiceB.GetMessage.IsValidMessage(testString);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        /// <summary>
+        /// Test with a string where the name is ommitted
+        /// </summary>
+        [Fact]
+        public void NoNameMessage()
+        {
+            // Arrange
+            string testString = "Hello my name is,";
+
+            // Act
+            bool result = ServiceB.GetMessage.IsValidMessage(testString);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        /// <summary>
+        /// Test with message that has invalid name
+        /// </summary>
+        [Fact]
+        public void InvalidNameMessage()
+        {
+            // Arrange
+            string testString = "Hello my name is, Ridgewell423";
+
+            // Act
+            bool result = ServiceB.GetMessage.IsValidMessage(testString);
+
+            // Assert
+            Assert.False(result);
+        }
+        #endregion
+
+        #region Get Name test
+
+        /// <summary>
+        /// Extracts name from valid string
+        /// </summary>
+        [Fact]
+        public void ExtractGetNameSuccessfully()
+        {
+            // Arrange
+            string testString = "Hello my name is, Ridgewell";
+
+            // Act
+            string name = ServiceB.GetMessage.GetName(testString);
+
+            // Assert
+            Assert.Equal("Ridgewell", name);
+        }
+
+
+        /// <summary>
+        /// Extracts name from invalid string, throws exception
+        /// </summary>
+        [Fact]
+        public void ExtractGetNameWithError()
+        {
+            // Arrange
+            string testString = "Wonga String";
+
+            // Act and Assert
+            Exception ex = Assert.Throws<ArgumentException>(() => ServiceB.GetMessage.GetName(testString));
+
+            // Assert
+            Assert.Equal(ex.Message, testString);
 
         }
         #endregion
