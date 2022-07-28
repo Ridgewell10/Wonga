df138b04950a71a35ae3ffe45defe5ef7e3c3901
Ridgewell <ridgewell@ava.solutions>
2022-07-28 20:41:00 +0200

    service a skeleton

--- ServiceA/Program.cs
+++ ServiceA/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ServiceA
+{
+    class Program
+    {
+        private const string QUEUE_NAME = "myQueue";
+        static void Main(string[] args)
+        {
+            string name = string.Empty;
+            Publisher queuePublisher = new Publisher(QUEUE_NAME);
+            do
+            {
+                Console.Write("Enter a name ['q' to quit]: ");
+                name = Console.ReadLine();
+                if (!string.IsNullOrEmpty(name) && name.ToLower() != "q")
+                {
+                    queuePublisher.PublishMessage($"Hello my name is, {name}");
+                    Console.Clear();
+                    Console.WriteLine("Published Message: {0}", name);
+                }
+            } while (name.ToLower() != "q");
+        }
+    }
+
+}
---
+++ ServiceA/Publisher.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceA
+{
+    /// <summary>
+    /// Used to publish to RabbitMQ Queue
+    /// </summary>
+    public class Publisher
+    {
+        private readonly string queueName;
+        private readonly ConnectionFactory connectionFactory = null;
+        private const string HOST_NAME = "localhost";
+
+        /// <summary>
+        /// Constructor for QueueuPublisher object
+        /// </summary>
+        /// <param name="queueName">The name of the Queue that object should publish to</param>
+        public Publisher(string queueName)
+        {
+            this.queueName = queueName;
+            connectionFactory = new ConnectionFactory() { HostName = HOST_NAME };
+        }
+
+        /// <summary>
+        /// Publish a message to a queue
+        /// </summary>
+        /// <param name="message">The message to be published</param>
+        public void PublishMessage(string message)
+        {
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queueName, false, false, false, null);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("", queueName, null, body);
+            }
+        }
+    }
+}
